c92b428bb3a7f6fbf194eb94199635d669b822bb
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-10-16 18:47:39 -0400

    added year feature

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-const form = document.querySelector("form");
+//Constructores
+
+//Usaremos 2 objetos en general
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI() {
+    
+}
+
+//Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+        min = max - 23;
+
+        const selectYear = document.querySelector("#year");
+        
+        for (let index = max; index >= min; index--) {
+            let option = document.createElement("option");
+            option.value = index;
+            option.textContent = index;
+            selectYear.appendChild(option);
+            
+        }
+}
+
+//Instanciamos UI
+const ui = new UI();
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    ui.llenarOpciones(); //Llena el select con los años
+});
