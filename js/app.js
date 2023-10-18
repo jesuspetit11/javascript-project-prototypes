372a5983b1fe1b31854e4d8abedbdc28124b6db9
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-10-17 23:55:30 -0400

    validating form

--- js/app.js
+++ js/app.js
@@ -34,3 +34,27 @@ const ui = new UI();
 document.addEventListener("DOMContentLoaded", ()=>{
     ui.llenarOpciones(); //Llena el select con los años
 });
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) { //Como es un submit tomamos el evento
+    e.preventDefault();
+    //Leer la marca seleccionada
+    const marca = document.querySelector("#marca").value;
+    //Leer la el año seleccionado
+    const year = document.querySelector("#year").value;
+    //Leer el tipo de covertura seleccionado
+    const tipo = document.querySelector("input[name = tipo]:checked").value;
+    console.log(tipo);
+    console.log("Cotizando");
+
+    if(marca === "" || year === "" || tipo === ""){
+        console.log("No pasó la validación");
+    } else {
+        console.log("Si pasó la validación");
+    }
+}
